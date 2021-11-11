7852a25ee8ef83e519e004df7ffbf069f75a1593
abdullahtech11 <92114680+abdullahtech11@users.noreply.github.com>
2021-11-12 02:48:19 +0500

    Update operator.js
    connect with heroku node js server

--- operator.js
+++ operator.js
@@ -6,7 +6,7 @@ function post_create()
     let age = document.getElementById('age').value;
     let roll_no = document.getElementById('roll_no').value;
 
-    axios.post('https://pacific-coast-71393.herokuapp.com/user', {
+    axios.post('https://node-js-server-abd.herokuapp.com/user', {
         student_name: student_name,father_name:father_name
         ,age:age,roll_no:roll_no
           })
@@ -25,7 +25,7 @@ function post_create()
 }
 
 function get_all(){
-    axios.get('https://pacific-coast-71393.herokuapp.com/users')
+    axios.get('https://node-js-server-abd.herokuapp.com/users')
   .then(function (response) {
       $html='';
     console.log(response);
@@ -78,7 +78,7 @@ function update_student(){
   let roll_no = document.getElementById('roll_no').value;
 
   let id = document.getElementById('student_id').value;
-  axios.put('https://pacific-coast-71393.herokuapp.com/user/'+id, {
+  axios.put('https://node-js-server-abd.herokuapp.com/user/'+id, {
     student_name: student_name,father_name:father_name
     ,age:age,roll_no:roll_no
 })
@@ -93,7 +93,7 @@ function update_student(){
 }
 function delete_student(){
     let id = document.getElementById('student_id').value;
-    axios.delete('https://pacific-coast-71393.herokuapp.com/user/'+id)
+    axios.delete('https://node-js-server-abd.herokuapp.com/user/'+id)
   .then(function (response) {
     console.log(response);
     alert(response.data)
